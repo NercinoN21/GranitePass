c928622c5a79c2e56a13ab097218931974dcd425
Nercino Neto <njbneto21@gmail.com>
2024-03-04 00:58:06 -0300

    App builder

--- granitepass/__init__.py
+++ granitepass/__init__.py
@@ -1,3 +1,15 @@
 """
 Routine responsible for adding configurations and registering blueprints.
 """
+from flask import Flask
+from granitepass.config import Config
+from granitepass.routes import RouteHandler
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    route_handler = RouteHandler()
+    route_handler.add_routes()
+    app.register_blueprint(route_handler.main)
+    return app
