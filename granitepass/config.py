15e3e7b3437375bbde5711f03ac567e449a164e0
Joao-Pedro-BS <134407785+Joao-Pedro-BS@users.noreply.github.com>
2024-03-04 00:30:29 -0300

    Adding Config for Login Page

--- granitepass/config.py
+++ granitepass/config.py
@@ -12,6 +12,9 @@ class Config:
         self.__data_dir = os.getenv('DATA_DIR')
         self.__data_file = os.getenv('DATA_FILE')
         self.__tables = os.getenv('TABLES')
+        self.__password = os.getenv('PASSWORD')
+        self.__user = os.getenv('USER')
+        self.__enter_btn = os.getenv('ENTER_BTN')
 
     def __str__(self):
         return f"{self.__dict__}"
@@ -35,3 +38,15 @@ class Config:
     @property
     def db_file(self):
         return os.path.join(self.data_dir, self.data_file)
+
+    @property
+    def password(self):
+        return self.__password
+    
+    @property
+    def user(self):
+        return self.__user
+    
+    @property
+    def enter_btn(self):
+        return self.__enter_btn
